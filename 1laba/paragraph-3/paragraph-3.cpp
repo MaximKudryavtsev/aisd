3ec7749a99257d64b7aaf0a54c1b5f8214bf5941
MaximKudryavtsev <maks07031997@yandex.ru>
2017-01-06 17:50:44 +0300

    fixed

--- 1laba/paragraph-3/paragraph-3.cpp
+++ 1laba/paragraph-3/paragraph-3.cpp
@@ -1,100 +1,81 @@
 /*
-    10. Имеется текстовый файл с некоторым описанием. Переносов
-    слов нет.  Требуется каждые 3 предложения выделить в отдельный
-    абзац.  Каждое   предложение   может   заканчиваться   точкой,
-    вопросительным либо восклицательным знаком (6).
+ 10. Имеется текстовый файл с некоторым описанием. Переносов
+слов нет.  Требуется каждые 3 предложения выделить в отдельный
+абзац.  Каждое   предложение   может   заканчиваться   точкой,
+вопросительным либо восклицательным знаком (6).
 */
 
 #include "stdafx.h"
 
 using namespace std;
 
-bool IsEndOfFile(ifstream &inputFile)
+string getStringFromInput(ifstream &input)
 {
-	return inputFile.peek() == ifstream::traits_type::eof();
+	string str;
+	string result;
+	while (getline(input, str))
+	{
+		for (size_t i = 0; i < str.length(); ++i)
+		{
+			result += str[i];
+		}
+	}
+	return result;
 }
 
-void FragmentationOnParagraph(ifstream &inputFile)
+void divisionOnParagraphs(string &oneString, ofstream &output)
 {
-	string line;
-	getline(inputFile, line);
-	char* str = (char*)line.c_str();
-	//char str[] = "first. two? three! four.";
-	int i = 0;
-	char* pointer = strtok(str, ".!?");
-	while ((pointer != NULL) && (pointer != " "))
+	int counter = 0;
+	bool flag = false;
+	string finalString;
+	for (size_t i = 0; i < oneString.length(); i++)
 	{
-		
-		if (i % 3 == 0) 
+		if ((flag) && (isspace(static_cast<unsigned char>(oneString[i]))))
 		{
-			cout << endl;
+			continue;
 		}
-		i++;
-		cout << pointer;
-		pointer = strtok(NULL, ".!?");
+		else if (oneString[i] != ' ')
+		{
+			flag = false;
+		}
+		finalString += oneString[i];
+		if ((oneString[i] == '.') || (oneString[i] == '?') || (oneString[i] == '!'))
+		{
+			++counter;
+		}
+		cout << counter;
+		output << finalString;
+		if (counter == 3)
+		{
+			output << "\n";
+			finalString.clear();
+			counter = 0;
+			flag = true;
+		}
+		finalString.clear();
 	}
+	
 }
 
 int main(int argc, char* argv[])
 {
-    setlocale(LC_ALL, "Russian");
+	setlocale(LC_ALL, "Russian");
 
-    if (argc != 2)
-    {
-        std::cerr << "Invalid amount of arguments\n"
-             << "Usage: paragraph-3.exe <input>" << endl;
-        return 1;
-    }
+	if (argc != 3)
+	{
+		std::cerr << "Invalid amount of arguments\n"
+			<< "Usage: paragraph-3.exe <input>" << endl;
+		return 1;
+	}
 
-    ifstream file(argv[1]);
-
-    if (!file.is_open())
-    {
-        std::cerr << "Failed to open " << argv[1] << " for reading!" << std::endl;
-        return 1;
-    }
-
-    std::string str;
-    std::string result;
-
-    while (std::getline(file, str))
-    {
-        int counter = 0;
-        bool flag = false;
-
-        for (size_t i = 0; i < str.length(); ++i)
-        {
-            // пропускаем пробелы в новой строке после того как нашли 3 знака
-            if ((flag) && (isspace(static_cast<unsigned char>(str[i]))))
-            {
-                continue;
-            }
-            else if (str[i] != ' ')
-            {
-                flag = false;
-            }
-
-            result += str[i];
-
-            if ((str[i] == '.') || (str[i] == '?') || (str[i] == '!'))
-            {
-                ++counter;
-            }
-
-            if (counter == 3)
-            {
-                std::cout << result << std::endl;
-
-                result.clear();
-                counter = 0;
-
-                // ставим флажок, что нужно пропускать пробелы в начале новой строки
-                flag = true;
-            }
-        }
-
-        result.clear();
-    }
-
-    file.close();
+	ifstream input(argv[1]);
+	ofstream output(argv[2]);
+	if (!input.is_open())
+	{
+		cerr << "Failed to open " << argv[1] << " for reading!" << endl;
+		return 1;
+	}
+	string oneString = getStringFromInput(input);
+	divisionOnParagraphs(oneString, output);
+	input.close();
 }
