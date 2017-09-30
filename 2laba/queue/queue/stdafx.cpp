0b91d15e340c97040827ff54e14b0db5a0ce6057
MaximKudryavtsev <maks07031997@yandex.ru>
2017-09-30 11:50:41 +0300

    queue lab

--- 2laba/queue/queue/stdafx.cpp
+++ 2laba/queue/queue/stdafx.cpp
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
+// stdafx.cpp : source file that includes just the standard includes
+// queue.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
+=======
 // stdafx.cpp: исходный файл, содержащий только стандартные включаемые модули
 // queue.pch будет предкомпилированным заголовком
 // stdafx.obj будет содержать предварительно откомпилированные сведения о типе
@@ -6,3 +16,4 @@
 
 // TODO: Установите ссылки на любые требующиеся дополнительные заголовки в файле STDAFX.H
 // , а не в данном файле
+>>>>>>> origin/master
--- 2laba/queue/queue/stdafx.h
+++ 2laba/queue/queue/stdafx.h
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+=======
 // stdafx.h: включаемый файл дл€ стандартных системных включаемых файлов
 // или включаемых файлов дл€ конкретного проекта, которые часто используютс€, но
 // не часто измен€ютс€
+>>>>>>> origin/master
 //
 
 #pragma once
@@ -16,7 +22,15 @@
 #include <map>
 #include <vector>
 #include <sstream>
+<<<<<<< HEAD
+#include <algorithm>  
+
+
+
+// TODO: reference additional headers your program requires here
+=======
 
 
 
 // TODO: ”становите здесь ссылки на дополнительные заголовки, требующиес€ дл€ программы
+>>>>>>> origin/master
